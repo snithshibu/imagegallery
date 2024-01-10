a10981783f810288003bbb7e9d5e9166acd435b6
Snith Shibu <153205112+snithshibu@users.noreply.github.com>
2024-01-10 20:04:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -18,28 +18,36 @@ const alts = {
 
 /* Looping through images */
 
-for (const image of images) {
+for (const image of images) 
+{
   const newImage = document.createElement('img');
   newImage.setAttribute('src', `images/${image}`);
   newImage.setAttribute('alt', alts[image]);
   thumbBar.appendChild(newImage);
-  newImage.addEventListener('click', (e) => {
+  newImage.addEventListener('click', (e) => 
+    {
     displayedImage.src = e.target.getAttribute('src');
     displayedImage.alt = e.target.alt;
-  });
+  }
+                           );
 }
 
 /* Wiring up the Darken/Lighten button */
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', () => 
+  {
   const btnClass = btn.getAttribute('class');
-  if (btnClass === 'dark') {
+  if (btnClass === 'dark') 
+  {
     btn.setAttribute('class', 'light');
     btn.textContent = 'Lighten';
     overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-  } else {
+  } 
+  else 
+  {
     btn.setAttribute('class', 'dark');
     btn.textContent = 'Darken';
     overlay.style.backgroundColor = 'rgba(0,0,0,0)';
   }
-});
+}
+                    );
